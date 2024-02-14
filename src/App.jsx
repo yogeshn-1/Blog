15db43518b0b3171b2ae1ae61e4e9afbe5161ad0
yogeshn-1 <nirmalkary@gmail.com>
2024-02-14 12:29:21 +0530

    Testing services , basic components and install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth_service";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return <div>CleanUp</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return loading ? (
+    <div>Loading.....</div>
+  ) : (
+    <div className="h-screen flex flex-col bg-slate-500 items-center">
+      <Header />
+      <main>Test</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/appwrite/auth_service.js
+++ src/appwrite/auth_service.js
@@ -38,7 +38,7 @@ export class AuthService {
     try {
       return await this.account.get();
     } catch (error) {
-      throw error;
+      console.log(error);
     }
     return null;
   }
