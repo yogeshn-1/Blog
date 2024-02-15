ff2e3595c7075b083dbbb9c5e813ef19e13a2e89
yogeshn-1 <nirmalkary@gmail.com>
2024-02-15 12:27:59 +0530

    Auth layout wrapper to protect user from navigating to restricted path

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Protected = ({ children, authentication = "true" /*default*/ }) => {
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.authSlice.status);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      useNavigate("/");
+    }
+    setIsLoading(false);
+  }, [navigate, authStatus, authentication]);
+  return isLoading ? <h1>Loading....</h1> : <>{children}</>;
+};
+
+export default Protected;
